c908ad3ffdeae43ac321e35daed13b1113292ec1
DavidPC <davidcuartas22@gmail.com>
2025-09-24 09:04:10 -0500

    Se implementa busqueda de usuarios oara validar inicio de sesion, intalacion de libreria Sweetalert2

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.querySelector("#login");
+import { Usuarios } from "./src/config/database.js";
+import { alertaRedireccion } from "./src/utils/aletas.js";
+
+let btnLogin = document.querySelector("#login"); //Selecciono el boton en una variable, se convierte en un objeto
+btnLogin.addEventListener("click", ()=>{
+    let correo = document.querySelector("#email").value; //Selecciono el valor del input
+    let contrasena = document.querySelector("#password").value;
+    let usuarioEncontrado = Usuarios.find((usuario)=> correo == usuario.correo && contrasena == usuario.contraseña) //Busca en la base de datos si existe el usuario
+    if (usuarioEncontrado){
+        return alertaRedireccion()
+    }
+    return alertaRedireccion()
+}) //Agrega una accion al boton y esta atento a un evento - Se agrega una funcion flecha
---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+// Simulación de una base de datos de usuarios
+export const Usuarios = [
+  {
+    nombre: "Juan Pérez",
+    correo: "juan.perez@example.com",
+    id: "USR001A",
+    contraseña: "password123",
+    rol: "admin"
+  },
+  {
+    nombre: "Ana Gómez",
+    correo: "ana.gomez@example.com",
+    id: "USR002B",
+    contraseña: "securepass456",
+    rol: "usuario"
+  },
+  {
+    nombre: "Luis Torres",
+    correo: "luis.torres@example.com",
+    id: "USR003C",
+    contraseña: "mypassword789",
+    rol: "usuario"
+  }
+];
